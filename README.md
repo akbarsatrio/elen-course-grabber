2b5faa8e46a4cea98607baac07ce562a92ec3ba0
akbarsatrio <akbarsatrio@outlook.co.id>
2021-03-19 13:10:31 +0700

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+#Elen Course Grabber
+<hr>
+####Instalasi
+
+1.  Install PhantomJS di Lokal
+    - Download Phantom JS
+    `wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2`
+    - Ekstrak / Untar file
+    `tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2`
+    - Copy ke folder bin
+    `cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/`
+<br>
+2.  Menjalankan File **main.js**
+    - Buka terminal, lalu ketik
+    `phantomjs main.js <username elen> <password elen>`
+<br>
+3.  Tunggu beberapa saat dan course yang ada di elen sudah di screenshot dan disimpan di dalam satu folder dengan **main.js**
--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 var page = require('webpage').create();
+var today = new Date();
 page.viewportSize = { width: 1366, height: 768 };
 page.open('https://elen.nurulfikri.ac.id/login/index.php', function(){
-  page.includeJs('https://code.jquery.com/jquery-3.6.0.slim.js', function(){
-    var system = require('system');
-    page.evaluate(function(system){
-      $('#username').val(system.args[1]);
-      $('#password').val(system.args[2]);
-      $('#login').submit();
-    }, system);
-    setInterval(function(){
-      page.open("https://elen.nurulfikri.ac.id/my/index.php?mynumber=-2", function(){
-        page.evaluate(function(){
-          var cList = document.getElementsByClassName('course_list');
-          document.write(cList[0].innerHTML);
-          document.body.style.background='#FFFFFF';
-        });
-        page.render('course-list.png');
-        phantom.exit();
+  var system = require('system');
+  page.evaluate(function(system) {
+    document.getElementById('username').value = system.args[1];
+    document.getElementById('password').value =system.args[2];
+    document.getElementById('login').submit();
+  }, system);
+  setInterval(function(){
+    page.open("https://elen.nurulfikri.ac.id/my/index.php?mynumber=-2", function(){
+      page.evaluate(function(){
+        var cList = document.getElementsByClassName('course_list');
+        document.write(cList[0].innerHTML);
+        document.body.style.background='#FFFFFF';
       });
-    }, 5000);
-  })
+      page.render('course-list-['+today.getDate()+'-'+today.getMonth()+'-'+today.getFullYear()+'].png');
+      phantom.exit();
+    });
+  }, 5000);
 });
